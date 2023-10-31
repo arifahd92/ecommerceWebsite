9d415eb6d5c54475bb9d710cc5a6dedd135d1d5a
md arif <arifahd92@gmail.com>
2023-10-31 22:44:31 +0530

    random fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,43 @@
 import React from "react";
-
 import classes from "./Footer.module.css";
+
+// Import Bootstrap icons
+import { SiYoutube, SiSpotify, SiFacebook } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer className={classes.footer}>
-      <span>The Generics</span>
-      <div className={classes.links}>
-        <a rel="noreferrer" href="https://www.youtube.com" target="_blank">
-          <img
-            src="https://connecting-to-database-c9c11.web.app/footer/youtube.png"
-            alt="YouTube"
-          />
-        </a>
-        <a rel="noreferrer" href="https://www.spotify.com" target="_blank">
-          <img
-            src="https://connecting-to-database-c9c11.web.app/footer/spotify.png"
-            alt="Spotify"
-          />
-        </a>
-        <a rel="noreferrer" href="https://www.facebook.com" target="_blank">
-          <img
-            src="https://connecting-to-database-c9c11.web.app/footer/facebook.png"
-            alt="Facebook"
-          />
-        </a>
+    <footer>
+      <div className="container">
+        <div className="row bg-secondary ">
+          <div className="col-md-8 text-center text-md-left ">
+            <h1>The Generics</h1>
+          </div>
+          <div className="col-md-4 border ">
+            <div className="d-flex justify-content-between mt-md-3  mb-2">
+              <a
+                rel="noreferrer"
+                href="https://www.youtube.com"
+                target="_blank">
+                <SiYoutube size={32} />
+              </a>
+              <a
+                rel="noreferrer"
+                href="https://www.spotify.com"
+                target="_blank">
+                <SiSpotify size={32} />
+              </a>
+              <a
+                rel="noreferrer"
+                href="https://www.facebook.com"
+                target="_blank">
+                <SiFacebook size={32} /> {/* Facebook icon */}
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
 };
+
 export default Footer;
